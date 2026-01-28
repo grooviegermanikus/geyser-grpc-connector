4e3d6b92d6bada8d9f4546a4ab1c0148b5b34db0
GroovieGermanikus <grooviegermanikus@gmail.com>
2026-01-28 13:35:05 +0100

    compare

--- examples/stream_tx_status.rs
+++ examples/stream_tx_status.rs
@@ -6,16 +6,19 @@ use geyser_grpc_connector::{
 use log::info;
 use solana_commitment_config::CommitmentConfig;
 use solana_signature::Signature;
-use std::collections::HashMap;
+use std::collections::{BTreeMap, HashMap};
 use std::env;
 use std::str::FromStr;
 use std::time::Duration;
+use itertools::Itertools;
+use solana_clock::Slot;
 use solana_pubkey::Pubkey;
+use tokio::join;
 use tokio::sync::broadcast;
 use tonic::transport::ClientTlsConfig;
 use yellowstone_grpc_proto::geyser::subscribe_update::UpdateOneof;
-use yellowstone_grpc_proto::geyser::{SubscribeRequest, SubscribeRequestFilterSlots, SubscribeRequestFilterTransactions};
-use geyser_grpc_connector::geyser_loop_but_cooler::GeyerLoopButCooler;
+use yellowstone_grpc_proto::geyser::{SubscribeRequest, SubscribeRequestFilterSlots, SubscribeRequestFilterTransactions, SubscribeUpdate};
+use geyser_grpc_connector::geyser_loop_but_cooler::GeyserLoopButCooler;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -52,7 +55,8 @@ pub async fn main() {
         timeouts.clone(),
     );
 
-    let (autoconnect_tx, mut slots_rx) = tokio::sync::mpsc::channel(10);
+    let (autoconnect1_tx, mut messages1_rx) = tokio::sync::mpsc::channel(10);
+    let (autoconnect2_tx, mut messages2_rx) = tokio::sync::mpsc::channel(10);
 
     let (_exit, exit_notify) = broadcast::channel(1);
 
@@ -62,52 +66,124 @@ pub async fn main() {
     let _green_stream_ah = create_geyser_autoconnection_task_with_mpsc(
         green_config.clone(),
         build_tx_status_subscription(my_wallet),
-        autoconnect_tx.clone(),
+        autoconnect1_tx.clone(),
         exit_notify.resubscribe(),
     );
 
-    // let _blue_stream_ah = create_geyser_autoconnection_task_with_mpsc(
-    //     blue_config.clone(),
-    //     build_tx_status_subscription(),
-    //     autoconnect_tx.clone(),
-    //     exit_notify.resubscribe(),
-    // );
+    let _blue_stream_ah = create_geyser_autoconnection_task_with_mpsc(
+        green_config.clone(),
+        build_tx_status_subscription_cool(my_wallet),
+        autoconnect2_tx.clone(),
+        exit_notify.resubscribe(),
+    );
 
-    let mut cool = GeyerLoopButCooler::new();
 
-    '_recv_loop: loop {
-        match slots_rx.recv().await {
-            Some(Message::GeyserSubscribeUpdate(update)) => {
 
-                let what_to_do = cool.consume(&update);
-                match what_to_do {
-                    geyser_grpc_connector::geyser_loop_but_cooler::Effect::EmitConfirmedMessages(sigs) => {
-                        for sig in sigs {
-                            info!("Emitting confirmed tx: {:?}", sig);
+    let task1 = tokio::spawn(async move {
+
+        let mut count_per_slot: BTreeMap<Slot, u32> = BTreeMap::new();
+
+        '_recv_loop: loop {
+            match messages1_rx.recv().await {
+                Some(Message::GeyserSubscribeUpdate(update)) => {
+
+                    match update.update_oneof {
+                        Some(UpdateOneof::TransactionStatus(msg)) => {
+                            let slot = msg.slot;
+                            let sig = Signature::try_from(msg.signature.as_slice()).unwrap();
+
+                            println!("tx status {}: {}", slot, sig);
+
+                            if !count_per_slot.contains_key(&slot) {
+                                if let Some(last) = count_per_slot.iter().max() {
+                                    println!("slot {} had {} tx statuses", last.0, last.1);
+                                }
+                            }
+
+                            count_per_slot.entry(slot).and_modify(|c| *c += 1).or_insert(1);
+
                         }
-                    }
-                    geyser_grpc_connector::geyser_loop_but_cooler::Effect::Noop => {}
+                        Some(_) => {}
+                        None => {}
+                    }},
+                None => {
+                    log::warn!("multiplexer channel closed - aborting");
+                    return;
                 }
-
-                match update.update_oneof {
-                    Some(UpdateOneof::TransactionStatus(msg)) => {
-                        let sig = Signature::try_from(msg.signature.as_slice()).unwrap();
-
-                        println!("Received tx status: slot {}, tx: {:?}", msg.slot, sig);
-                    }
-                    Some(_) => {}
-                    None => {}
-                }},
-            None => {
-                log::warn!("multiplexer channel closed - aborting");
-                return;
+                Some(Message::Connecting(_)) => {}
             }
-            Some(Message::Connecting(_)) => {}
         }
-    }
+
+    });
+
+
+    let task2 = tokio::spawn(async move {
+
+
+        let mut cool = GeyserLoopButCooler::new();
+
+        '_recv_loop: loop {
+            match messages2_rx.recv().await {
+                Some(Message::GeyserSubscribeUpdate(update)) => {
+
+                    let what_to_do = cool.consume(*update);
+                    match what_to_do {
+                        geyser_grpc_connector::geyser_loop_but_cooler::Effect::EmitConfirmedMessages { confirmed_slot, grpc_messages } => {
+                            println!("cool: slot {} had {} tx statuses", confirmed_slot, grpc_messages.len());
+                            for msg in grpc_messages {
+
+                                match msg.update_oneof {
+                                    Some(UpdateOneof::TransactionStatus(msg)) => {
+                                        let slot = msg.slot;
+                                        let sig = Signature::try_from(msg.signature.as_slice()).unwrap();
+                                        println!("cool tx {}: {:?}", slot, sig);
+                                    }
+                                    _ => {}
+                                }
+                            }
+                        }
+                        geyser_grpc_connector::geyser_loop_but_cooler::Effect::Noop => {}
+                    }
+
+                },
+                None => {
+                    log::warn!("multiplexer channel closed - aborting");
+                    return;
+                }
+                Some(Message::Connecting(_)) => {}
+            }
+        }
+
+    });
+
+    join!(task1, task2);
+
 }
 
 fn build_tx_status_subscription(_wallet: Pubkey) -> SubscribeRequest {
+    let mut transactions_status_subs = HashMap::new();
+    transactions_status_subs.insert(
+        "client".to_string(),
+        SubscribeRequestFilterTransactions {
+            vote: Some(false),
+            // include failed tx as we shouldn't send them again
+            failed: None,
+            signature: None,
+            account_include: vec![],
+            account_exclude: vec![],
+            account_required: vec![],
+        },
+    );
+
+    SubscribeRequest {
+        transactions_status: transactions_status_subs,
+        commitment: Some(map_commitment_level(CommitmentConfig::confirmed()) as i32),
+        ..Default::default()
+    }
+}
+
+
+fn build_tx_status_subscription_cool(_wallet: Pubkey) -> SubscribeRequest {
 
     let mut important_slots_sub = HashMap::new();
 
@@ -136,7 +212,7 @@ fn build_tx_status_subscription(_wallet: Pubkey) -> SubscribeRequest {
     SubscribeRequest {
         slots: important_slots_sub,
         transactions_status: transactions_status_subs,
-        commitment: Some(map_commitment_level(CommitmentConfig::confirmed()) as i32),
+        commitment: Some(map_commitment_level(CommitmentConfig::processed()) as i32),
         ..Default::default()
     }
 }
--- src/geyser_loop_but_cooler.rs
+++ src/geyser_loop_but_cooler.rs
@@ -8,10 +8,11 @@ use yellowstone_grpc_proto::geyser::subscribe_update::UpdateOneof;
 use yellowstone_grpc_proto::prelude::SubscribeUpdateTransactionStatus;
 
 pub struct MessagesBuffer {
-    tx_status_messages: Vec<Signature>,
+    // TODO consolidate naming: grpc messages vs updates
+    grpc_messages: Vec<SubscribeUpdate>,
 }
 
-pub struct GeyerLoopButCooler {
+pub struct GeyserLoopButCooler {
 
     skip_til_this_slot: Option<Slot>,
     buffer: HashMap<Slot, MessagesBuffer>,
@@ -20,11 +21,12 @@ pub struct GeyerLoopButCooler {
 }
 
 pub enum Effect {
-    EmitConfirmedMessages(Vec<Signature>),
+    // confimred slot + messages
+    EmitConfirmedMessages { confirmed_slot: Slot, grpc_messages: Vec<SubscribeUpdate> },
     Noop,
 }
 
-impl GeyerLoopButCooler {
+impl GeyserLoopButCooler {
 
     pub fn new() -> Self {
         Self {
@@ -33,19 +35,31 @@ impl GeyerLoopButCooler {
         }
     }
 
-    pub fn consume(&mut self, update: &SubscribeUpdate) -> Effect {
+    pub fn foobar(&mut self) {
+    }
+
+    pub fn consume(&mut self, update: SubscribeUpdate) -> Effect {
 
         match update.update_oneof.as_ref() {
             // this is important
             Some(UpdateOneof::Slot(msg)) => {
-                let commitment_status = CommitmentLevel::try_from(msg.status).unwrap();
-                if self.skip_til_this_slot == None && commitment_status == CommitmentLevel::Processed {
+                let commitment_status = SlotStatus::try_from(msg.status).expect("status");
+
+                if self.skip_til_this_slot == None && commitment_status == SlotStatus::SlotProcessed {
+                    // special case for proper startup
                     self.skip_til_this_slot = Some(msg.slot);
                     return Effect::Noop;
                 }
-                if commitment_status != CommitmentLevel::Confirmed {
+                if commitment_status == SlotStatus::SlotProcessed {
+                    // make sure the messages is there even if it's empty
+                    self.buffer.entry(msg.slot)
+                        .or_insert_with(|| MessagesBuffer { grpc_messages: Vec::with_capacity(64) });
                     return Effect::Noop;
                 }
+                if commitment_status != SlotStatus::SlotConfirmed {
+                    return Effect::Noop;
+                }
+                let confirmed_slot = msg.slot;
                 if let Some(skip_slot) = self.skip_til_this_slot {
                     if msg.slot <= skip_slot {
                         println!("hold back until slot > {}", skip_slot);
@@ -56,22 +70,31 @@ impl GeyerLoopButCooler {
                     return Effect::Noop;
                 }
 
-                let messages = self.buffer.remove(&msg.slot).expect("must be there");
+                let messages = self.buffer.remove(&confirmed_slot).expect("must be there");
 
-                println!("Need to flush messages for slot {} {}", msg.slot, messages.tx_status_messages.len());
+                println!("Need to flush messages for slot {} {}", confirmed_slot, messages.grpc_messages.len());
 
-                return Effect::EmitConfirmedMessages(messages.tx_status_messages)
+                return Effect::EmitConfirmedMessages { confirmed_slot, grpc_messages: messages.grpc_messages  };
 
             }
-            Some(UpdateOneof::TransactionStatus(msg)) => {
-                let slot = msg.slot;
-                let sig = Signature::try_from(msg.signature.as_slice()).unwrap();
-                println!("Transaction status received for slot {} sig {}", slot, sig);
+            // all messages except slot (+ping pong)
+            Some(msg) => {
+
+                match msg {
+                    UpdateOneof::Ping(_) => {
+                        return Effect::Noop;
+                    }
+                    UpdateOneof::Pong(_) => {
+                        return Effect::Noop;
+                    }
+                    _ => {}
+                }
+
+                let slot = get_slot(&msg);
                 self.buffer.entry(slot)
-                    .or_insert_with(|| MessagesBuffer { tx_status_messages: Vec::with_capacity(64) })
-                    .tx_status_messages.push(sig);
+                    .or_insert_with(|| MessagesBuffer { grpc_messages: Vec::with_capacity(64) })
+                    .grpc_messages.push(update);
             }
-            Some(_) => {}
             None => {}
         }
 
@@ -81,19 +104,43 @@ impl GeyerLoopButCooler {
 
 }
 
-
+fn get_slot(update: &UpdateOneof) -> Slot {
+    match update {
+        UpdateOneof::Account(msg) => {
+            msg.slot
+        }
+        UpdateOneof::TransactionStatus(msg) => {
+            msg.slot
+        }
+        UpdateOneof::Transaction(msg) => {
+            msg.slot
+        }
+        UpdateOneof::Block(msg) => {
+            msg.slot
+        }
+        UpdateOneof::BlockMeta(msg) => {
+            msg.slot
+        }
+        UpdateOneof::Entry(msg) => {
+            msg.slot
+        }
+        _ => {
+            panic!("unsupported update type for get_slot: {:?}", update);
+        }
+    }
+}
 
 #[test]
 pub fn test_gesyer_loop_but_cooler() {
 
-    let mut cool = GeyerLoopButCooler::new();
+    let mut cool = GeyserLoopButCooler::new();
 
     let sig1 = Signature::from_str("2h6iPLYZEEt8RMY3gGFUqd4Jktrg2fYTCMffifRoQDJWPqLvZ1gRKqpq4e5s8kWrVigkyDXV6xEiw54zuChYBdyB").unwrap();
     let sig2 = Signature::from_str("5QE2kQUiMpv51seq4ShtoaAzdkMT7fzeQ5TvqTPFgNkcahtHSnZudindggjTUXt8uqZGifbWUAmUubdWLhFHz719").unwrap();
     let sig3 = Signature::from_str("KQzbyZMUq6ujZL6qxDW2EMNUugvzcpFJSdzTnmhsV8rYgqkwL9rc3uXg1FpGPNKaSJQLmKXTfezJoVdBLEhVa8F").unwrap();
 
     for sig in [sig1, sig2, sig3] {
-        let effect = cool.consume(&SubscribeUpdate {
+        let effect = cool.consume(SubscribeUpdate {
             filters: vec![],
             created_at: None,
             update_oneof: Some(UpdateOneof::TransactionStatus(SubscribeUpdateTransactionStatus {
@@ -107,7 +154,7 @@ pub fn test_gesyer_loop_but_cooler() {
         assert!(matches!(effect, Effect::Noop));
     }
 
-    let effect = cool.consume(&SubscribeUpdate {
+    let effect = cool.consume(SubscribeUpdate {
         filters: vec![],
         created_at: None,
         update_oneof: Some(UpdateOneof::Slot(SubscribeUpdateSlot {
@@ -119,7 +166,7 @@ pub fn test_gesyer_loop_but_cooler() {
     });
     assert!(matches!(effect, Effect::Noop));
 
-    let effect = cool.consume(&SubscribeUpdate {
+    let effect = cool.consume(SubscribeUpdate {
         filters: vec![],
         created_at: None,
         update_oneof: Some(UpdateOneof::Slot(SubscribeUpdateSlot {
